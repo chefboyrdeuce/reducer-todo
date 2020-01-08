d82349225e77f8d4280dbde228e519c9a8493e06
chefboyrdeuce <jpldeuce@gmail.com>
2020-01-07 20:40:48 -0500

    built reducer & initial state, setup state in component

---
+++ reducer-todo/src/reducers/index.js
@@ -0,0 +1,13 @@
+export const initialState = [
+    {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+    }
+];
+
+export function todoReducer (state, action){
+    return state;
+};
+
+
